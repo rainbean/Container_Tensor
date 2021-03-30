cdc7d9687f0516d768dc84800fe8e9fcafe8f30d
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2021-03-30 10:34:14 +0800

    missing jupyter files

---
+++ jupyter/startup.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+mpl.rcParams['figure.figsize'] = [20, 10]
+np.set_printoptions(linewidth=200)
